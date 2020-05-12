7ed61caa8b6df2bccd282e0e3706dd90cd659b2d
Sofia Orlova <mysofina@gmail.com>
2020-05-12 16:38:42 +0300

    doing footer

---
+++ components/common/Footer.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <el-row>
+      <el-col />
+    </el-row>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      footerNav: [
+        {
+          title: this.$t('footerNav.main.title'),
+          subMenu: [
+            {
+              title: this.$t(footerNav.main.subMenu.about),
+              pageIndex: 'about'
+            }
+          ]
+        }
+      ]
+    }
+  },
+  i18n: {
+    messages: {
+      ru: {
+        footerNav: {
+          main: {
+            title: 'Главное',
+            subMenu: {
+              about: 'О TMDb'
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
